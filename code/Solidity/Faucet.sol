7fe66cba690491a1ed6c18bb6c88c830f8dc2c7b
jannathundal <58568553+jannathundal@users.noreply.github.com>
2024-11-17 14:05:28 -0500

    Update Faucet.sol
    I've updated the file to move past the following error
    ```
    TypeError: "send" and "transfer" are only available for objects of type "address payable", not "address".
      --> faucet.sol:12:9:
       |
    12 |         msg.sender.transfer(withdraw_amount);
       |         ^^^^^^^^^^^^^^^^^^^
    ```

--- code/Solidity/Faucet.sol
+++ code/Solidity/Faucet.sol
@@ -1,8 +1,3 @@
-// SPDX-License-Identifier: CC-BY-SA-4.0
-
-// Version of Solidity compiler this program was written for
-pragma solidity 0.6.4;
-
 // Our first contract is a faucet!
 contract Faucet {
     // Accept any incoming amount
@@ -14,6 +9,6 @@ contract Faucet {
         require(withdraw_amount <= 100000000000000000);
 
         // Send the amount to the address that requested it
-        msg.sender.transfer(withdraw_amount);
+        payable(msg.sender).transfer(withdraw_amount);
     }
 }
